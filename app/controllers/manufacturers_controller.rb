aa85e2bd1c18ed2fc4282dc536e0e6f5b1d15369
wdhays2 <wdhays2@charter.net>
2016-02-06 18:18:51 -0800

    Work on sorting and searching.

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -24,25 +24,25 @@ class ManufacturersController < ApplicationController
   end
 
   def list
-    drivers = Driver # start using the Driver activerecord model
+    @drivers = Driver # start using the Driver activerecord model
     if params[:manufacturer_id].present?
-      drivers = drivers.where(manufacturer_id: params[:manufacturer_id])
+      @drivers = @drivers.where(manufacturer_id: params[:manufacturer_id])
     end
 
     if params[:sealed_f3].present?
-      drivers = drivers.where(sealed_f3: params[:sealed_f3])
+      @drivers = @drivers.where(sealed_f3: params[:sealed_f3])
     end
 
     if params[:sealed_volume].present?
-      drivers = drivers.where(sealed_volume: params[:sealed_volume])
+      @drivers = @drivers.where(sealed_volume: params[:sealed_volume])
     end
 
     if params[:rms_power].present?
-      drivers = drivers.where(rms_power: params[:rms_power])
+      @drivers = @drivers.where(rms_power: params[:rms_power])
     end
 
     if params[:x_max].present?
-      drivers = drivers.where(x_max: params[:x_max])
+      @drivers = @drivers.where(x_max: params[:x_max])
     end
 
     # if params[:ranges].present?
@@ -51,24 +51,31 @@ class ManufacturersController < ApplicationController
     # end
 
     page = (params[:start].to_i / params[:length].to_i) + 1
-    drivers = drivers.paginate(page: page, per_page: params[:length])
-    render json: format_for_data_tables(drivers, 20, 100) # drivers.total_entries from will_paginate
+    @drivers = @drivers.where(driver_search)
+    @drivers = @drivers.order(order_list)
+    @drivers = @drivers.paginate(page: page, per_page: params[:length])
   end
 
   private
 
-  def format_for_data_tables(drivers, records_filtered, total_records)
-    retval = { data: [], recordsFiltered: records_filtered, recordsTotal: total_records }
-    drivers.each do |driver|
-      retval[:data] << [
-        driver.model_no,
-        driver.sealed_f3,
-        driver.sealed_volume,
-        driver.high_frequency_limit,
-        driver.rms_power,
-        driver.x_max
-      ]
+  def order_list
+    column = case params['order']['0']['column']
+    when '0' then 'model_no'
+    when '1' then 'sealed_f3'
+    when '2' then 'sealed_volume'
+    when '3' then 'high_frequency_limit'
+    when '4' then 'rms_power'
+    when '5' then 'x_max'
+    else 'model_no'
     end
-    retval
+
+    dir = params['order']['0']['dir'] == 'asc' ? 'asc' : 'desc'
+    [column, dir].join(' ')
+  end
+
+  def driver_search
+    searched_phrase = params['search']['value'].strip
+    return '' if searched_phrase.blank?
+    ['LOWER(model_no) LIKE ?', "#{searched_phrase.downcase}%"]
   end
 end
